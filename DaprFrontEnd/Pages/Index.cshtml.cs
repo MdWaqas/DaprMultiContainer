3dd1e491d55e56e3f7e87582baf12d976b5d81b8
waqas-idrees-confiz <waqas.idrees@confiz.com>
2021-07-13 15:37:02 +0500

    Add project files.

---
+++ DaprFrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Dapr.Client;
+
+namespace DaprFrontEnd.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly DaprClient _daprClient;
+        public IndexModel(ILogger<IndexModel> logger, DaprClient daprClient)
+        {
+            _logger = logger;
+            _daprClient = daprClient ?? throw new ArgumentNullException(nameof(daprClient));
+        }
+
+        public async Task OnGet()
+        {
+            var forecasts =
+                await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(HttpMethod.Get, "daprbackend",
+                    "weatherforecast");
+
+            ViewData["WeatherForecastData"] = forecasts;
+        }
+    }
+}
